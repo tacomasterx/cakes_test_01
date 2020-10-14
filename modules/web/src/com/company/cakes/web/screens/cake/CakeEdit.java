b4d243857280bf2f25372f775d8823f57b0137d2
Tomas Carrillo <tacoman8918@gmail.com>
2020-10-14 17:06:01 -0500

    Added New exceptions for "Gratis" cakes

--- modules/web/src/com/company/cakes/web/screens/cake/CakeEdit.java
+++ modules/web/src/com/company/cakes/web/screens/cake/CakeEdit.java
@@ -1,6 +1,7 @@
 package com.company.cakes.web.screens.cake;
 
 import com.company.cakes.entity.PriceGroup;
+import com.haulmont.cuba.core.global.DataManager;
 import com.haulmont.cuba.gui.Dialogs;
 import com.haulmont.cuba.gui.components.DialogAction;
 import com.haulmont.cuba.gui.components.HasValue;
@@ -25,6 +26,8 @@ public class CakeEdit extends StandardEditor<Cake> {
     private PickerField<PriceGroup> priceGroupField;
     @Inject
     private Dialogs dialogs;
+    @Inject
+    private DataManager dataManager;
 
     @Subscribe("priceGroupField")
     public void onPriceGroupFieldFieldValueChange(PickerField.FieldValueChangeEvent<PriceGroup> event) {
@@ -37,22 +40,26 @@ public class CakeEdit extends StandardEditor<Cake> {
         //priceField.setEnabled(false);
         PriceGroup priceGroup = event.getValue();
         assert priceGroup != null;
-        if(priceGroup.getName().equals("Gratis"))
-             priceField.setVisible(false);
+        if(priceGroup.getName().equals("Gratis")){
+            priceField.setValue(new BigDecimal("0.0"));
+            priceField.setVisible(false);
+        }
          else
              priceField.setVisible(true);
     }
 
     @Subscribe
     public void onBeforeCommitChanges(BeforeCommitChangesEvent event) {
-        if (getEditedEntity().getPrice() == null) {
+        if (getEditedEntity().getPrice() == null || getEditedEntity().getPrice().equals(new BigDecimal("0.00"))) {
+            PriceGroup priceGroup = dataManager.loadValue("select e from cakes_PriceGorup e where e.name = 'Gratis'",
+                    PriceGroup.class).one();
             dialogs.createOptionDialog()
                     .withCaption("Question")
                     .withMessage("Are you sure this is a free product?")
                     .withActions(
                             new DialogAction(DialogAction.Type.YES).withHandler(e -> {
                                 getEditedEntity().setPrice(new BigDecimal("0.0"));
-
+                                getEditedEntity().setPriceGroup(priceGroup);
                                 // retry commit and resume action
                                 event.resume(commitChanges());
                             }),
