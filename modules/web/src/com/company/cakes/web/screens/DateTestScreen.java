3890ad5aa2a4eef84aa5b7ea082647762b132e04
Tomas Carrillo <tacoman8918@gmail.com>
2020-10-19 20:32:33 -0500

    Completed Rematar refrigerador button and method.

--- modules/web/src/com/company/cakes/web/screens/DateTestScreen.java
+++ modules/web/src/com/company/cakes/web/screens/DateTestScreen.java
@@ -1,7 +1,9 @@
 package com.company.cakes.web.screens;
 
+import com.company.cakes.entity.Cake;
 import com.company.cakes.entity.CakeInventory;
 import com.company.cakes.entity.Refrigerator;
+import com.company.cakes.entity.Store;
 import com.company.cakes.web.screens.cakeinventory.CakeInventoryBrowseByRefri;
 import com.haulmont.cuba.core.global.DataManager;
 import com.haulmont.cuba.core.global.View;
@@ -15,7 +17,7 @@ import com.haulmont.cuba.gui.model.DataComponents;
 import com.haulmont.cuba.gui.model.DataContext;
 import com.haulmont.cuba.gui.screen.*;
 import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
-import org.slf4j.Logger;
+//import org.slf4j.Logger;
 
 import javax.inject.Inject;
 import java.time.LocalDate;
@@ -34,80 +36,108 @@ public class DateTestScreen extends Screen {
     private DataComponents dataComponents;
     @Inject
     private DataContext dataContext;
-    @Inject
-    private Logger log;
+//    @Inject
+//    private Logger log; // Librería para mostrar datos en consola
     @Inject
     private TabSheet tabSheet;
+
     @Inject
     private UiComponents uiComponents;
     @Inject
     private ScreenBuilders screenBuilders;
 
-    List<Refrigerator> refrigerators;
+    private List<Refrigerator> refrigerators;
+
 
     private CollectionContainer<CakeInventory> cakeInventoriesManualDc;
     private CollectionLoader<CakeInventory> cakeInventoriesManualDl;
-    
+
+    private List<CollectionContainer<CakeInventory>> cakeInventoryDcArray;
+    private List<CollectionLoader<CakeInventory>> cakeInventoryDlArray;
 
     @Subscribe("btnShowDate")
-    public void onBtnShowDateClick(Button.ClickEvent event) {
-       
+    public void onBtnShowDateClick(Button.ClickEvent event) {// Botón usado simplemente para aprender a manejar las fechas
+                                                             // Se puede ignorar
         LocalDate localDate = LocalDate.now();
         String smolYear = "" + localDate.getYear() ;
         //log.info("Variable localDate = {}", "" + localDate.getDayOfMonth() + localDate.getMonthValue() + smolDate.substring(2) );
         String smolDate = "" + dateZerofill(localDate.getDayOfMonth()) + dateZerofill(localDate.getMonthValue()) + smolYear.substring(2);
-        log.info("Variable localDate = {}", smolDate);
+        //log.info("Variable localDate = {}", smolDate);
     }
 
     @Subscribe
-    public void onInit(InitEvent event) {
+    public void onInit(InitEvent event) { // Acciones que deben tomarse durante la inicialización
         refrigerators = dataManager.loadValue("select e from cakes_Refrigerator e", Refrigerator.class).list();
-        createManualLoader();
-        log.info("Variable view = {}", cakeInventoriesManualDc.getItems().size());
-
+        //log.info("Variable view = {}", cakeInventoriesManualDc.getItems().size());
+        createManualLoader(refrigerators.get(1));
         for (int i = 0; i < refrigerators.size(); i++) {
+
+
 
             Label label = componentsFactory.createComponent(Label.class);
             label.setValue("" + refrigerators.get(i).getName());
 
-            Table<CakeInventory> table = uiComponents.create(Table.of(CakeInventory.class));
+            Table<CakeInventory> table = uiComponents.create(Table.of(CakeInventory.class));  //Intento de creación manual de la tabla
             table.setItems(new ContainerTableItems<>(cakeInventoriesManualDc));
 
+               // Declaración de Components Factory requerida para crear tabs.
             VBoxLayout tabContent = componentsFactory.createComponent(VBoxLayout.class);
             tabContent.setSpacing(true);
             tabContent.setMargin(true, false, true, false);
 
-            tabContent.add(label);
+            tabContent.add(label);        //Agregar un elemento de UI a una tab.
             tabContent.add(table);
             tabContent.expand(table);
 
-            Button showScreenButton = uiComponents.create(Button.class);
+            Button showScreenButton = uiComponents.create(Button.class);   // Agregar un botón de forma "programática"
             showScreenButton.setCaption("Mostrar tabla");
             int finalI = i;
             showScreenButton.addClickListener(e -> showRefrigeratorInventory(finalI) );
             tabContent.add(showScreenButton);
 
 
-            TabSheet.Tab tab = tabSheet.addTab("tab" + i, tabContent);
-            tab.setCaption("" + refrigerators.get(i).getName());
+            TabSheet.Tab tab = tabSheet.addTab("tab" + i, tabContent);   //Crear una nueva tab y meterla a la tabSheet
+            tab.setCaption("" + refrigerators.get(i).getName());               //Cambiar el nombre desplegado de la tab.
 
         }
     }
 
-    private void createManualLoader() {
+    private void createManualLoader(Refrigerator refrigerator) { // Intento de crear un Data Loader para la tabla
 
         cakeInventoriesManualDc = dataComponents.createCollectionContainer(CakeInventory.class);
+
+        View view = new View(CakeInventory.class)
+                .addProperty("status")
+                .addProperty("cake", new View(Cake.class)
+                        .addProperty("name")
+                        .addProperty("priceGroup")
+                ).addProperty("store", new View(Store.class)
+                        .addProperty("name")
+                        .addProperty("code")
+                ).addProperty("refrigerator")
+                .addProperty("exported");
 
         cakeInventoriesManualDl = dataComponents.createCollectionLoader();
         cakeInventoriesManualDl.setContainer(cakeInventoriesManualDc);
         cakeInventoriesManualDl.setDataContext(dataContext);
-        cakeInventoriesManualDl.setQuery("select e from cakes_CakeInventory e");
-        cakeInventoriesManualDl.setView(View.MINIMAL);
+        cakeInventoriesManualDl.setQuery("select e from cakes_CakeInventory e where e.refrigerator.id = '" + refrigerator.getId() + "'");
+        cakeInventoriesManualDl.setView(view);
 
-        log.info("Variable view = {}", cakeInventoriesManualDc.getItems());
+       // log.info("Variable view = {}", cakeInventoriesManualDc.getItems());  //
     }
 
-    private String dateZerofill(Integer number){
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        refrigerators = dataManager.loadValue("select e from cakes_Refrigerator e", Refrigerator.class).list();
+     //   for (int i = 0; i < refrigerators.size(); i++) { }
+        cakeInventoriesManualDl.load();  // Esto parece mejorar un poco el asunto de las tablas pero causa un problema de
+        //IllegalStateException: Cannot get unfetched attribute [exported] from detached object com.company.cakes.entity.CakeInventory-04379a92-fd37-7ebb-2243-c59fedd99ff2 [detached].
+    }
+
+
+
+    private String dateZerofill(Integer number){ // Zerofill para las fechas, hay una copia en la parte de
+                                                 // Crear SKU con fecha y tipo de precio.
         if (number > 9) {
             return "" + number;
         }
@@ -115,12 +145,12 @@ public class DateTestScreen extends Screen {
     }
 
     @Subscribe("btnShowRefri")
-    public void onBtnShowRefriClick(Button.ClickEvent event) {
+    public void onBtnShowRefriClick(Button.ClickEvent event) { // Botón de test para mostrar el inventrario de un Refrigerador
 
         showRefrigeratorInventory(1);
     }
 
-    private void showRefrigeratorInventory(int index){
+    private void showRefrigeratorInventory(int index){  // Función para desplegar la screen CakeInventoryBrowseByRefri
         screenBuilders.screen(this)
                 .withScreenClass(CakeInventoryBrowseByRefri.class)
 //                .withAfterCloseListener(e -> {
@@ -131,4 +161,5 @@ public class DateTestScreen extends Screen {
                 .build()
                 .show();
     }
+
 }
--- modules/web/src/com/company/cakes/web/screens/cakeinventory/CakeInventoryBrowseByRefri.java
+++ modules/web/src/com/company/cakes/web/screens/cakeinventory/CakeInventoryBrowseByRefri.java
@@ -3,12 +3,20 @@ package com.company.cakes.web.screens.cakeinventory;
 import com.company.cakes.entity.CakeInventory;
 import com.company.cakes.entity.Refrigerator;
 import com.company.cakes.web.screens.InventoryByRefrigeratorOptions;
+import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.gui.BulkEditors;
+import com.haulmont.cuba.gui.actions.list.BulkEditAction;
+import com.haulmont.cuba.gui.app.core.bulk.ColumnsMode;
+import com.haulmont.cuba.gui.components.Action;
 import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.components.GroupTable;
 import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.screen.*;
 
 import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.Arrays;
+import java.util.Collections;
 
 @UiController("cakes_CakeInventory.browseByRefri")
 @UiDescriptor("cake-inventory-browse-by-refri.xml")
@@ -20,16 +28,23 @@ public class CakeInventoryBrowseByRefri extends StandardLookup<CakeInventory> im
     private CollectionLoader<CakeInventory> cakeInventoriesDl;
     @Inject
     private GroupTable<CakeInventory> cakeInventoriesTable;
+    @Named("cakeInventoriesTable.bulkEdit")
+    private BulkEditAction cakeInventoriesTableBulkEdit;
+    @Inject
+    private Metadata metadata;
+    @Inject
+    private BulkEditors bulkEditors;
     private Refrigerator refrigerator;
 
 
     @Subscribe
     public void onInit(InitEvent event) {
-        ScreenOptions options = event.getOptions();
+
+        ScreenOptions options = event.getOptions();           // Recibir screen options como parámetro
         if (options instanceof InventoryByRefrigeratorOptions) { // Verificar que las options sean de la clase requerida
-            Refrigerator refrigerator = ((InventoryByRefrigeratorOptions) options).getRefrigerator();
+            Refrigerator refrigerator = ((InventoryByRefrigeratorOptions) options).getRefrigerator(); //Tomar la option de clase Refrigerator
             //log.info("Variable foo = {}", store.getName());
-            this.refrigerator = refrigerator;
+            this.refrigerator = refrigerator; //Asignar ese valor a una variable de screen
         }
     }
 
@@ -45,5 +60,25 @@ public class CakeInventoryBrowseByRefri extends StandardLookup<CakeInventory> im
     public void onSelectAllClick(Button.ClickEvent event) {
         cakeInventoriesTable.selectAll();
     }
+
+    @Subscribe("remate")
+    public void onRemateClick(Button.ClickEvent event) {
+        cakeInventoriesTable.selectAll();
+        cakeInventoriesTableBulkEdit.execute();
+        cakeInventoriesTable.setSelected(Collections.emptyList());
+        this.closeWithDefaultAction()  ;
+    }
+
+    @Subscribe("cakeInventoriesTable.bulkEdit")
+    public void onCakeInventoriesTableBulkEdit(Action.ActionPerformedEvent event) {
+        bulkEditors.builder(metadata.getClassNN(CakeInventory.class), cakeInventoriesTable.getSelected(), this)
+                .withListComponent(cakeInventoriesTable)
+                .withColumnsMode(ColumnsMode.ONE_COLUMN)
+                .withIncludeProperties(Arrays.asList("status"))
+                .create()
+                .show();
+    }
+
+    
 
 }
